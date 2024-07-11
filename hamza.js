b1c3c377503b88302728315a1504249c55815abe
HAMZA BADDI <hazmzabaddi47@gmail.com>
2024-07-12 00:59:16 +0200

    add readme file

--- hamza.js
+++ hamza.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('this is hamza laptop!')
+  res.send('this is hamza laptop! all is working')
 })
 
 app.listen(port, () => {
